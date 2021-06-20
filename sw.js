8fb7a3a524793db1ff1bc6311a4630681185c45f
PioBarGit <bartkowiakpiotr0@gmail.com>
2021-06-20 09:50:25 +0200

    first script

---
+++ sw.js
@@ -0,0 +1,21 @@
+var cacheName = "podyplomowe"
+var filesToCache = ["./","./index.html","./style.css","./js/main.js"];
+
+self.addEventListener("install", function (event){
+    event.waitUntil(
+        caches.open(cacheName).then( function(cache){
+            return cache.addAll(filesToCache)
+        })
+    )
+})
+
+self.addEventListener("fetch", function(event){
+    event.respondWith(
+        caches.match(event.request).then(function (response){
+            if (response) {
+                return response 
+            }
+            return fetch(event.request)
+        })
+    )
+})
